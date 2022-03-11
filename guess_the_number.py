1bc1c066f2d864c0fdb3a28997a0dba1a4294b38
Sim <36443813+simtb@users.noreply.github.com>
2022-03-11 18:31:22 +0000

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -10,13 +10,11 @@ if __name__ == "__main__":
     while user_guessed_number != random_number:
         if user_guessed_number < random_number:
             print("Unlucky! The number I'm thinking off is greater")
-            print("Try again... please enter a number:")
-            user_guessed_number = int(input())
-            number_of_guesses += 1
         else:
             print("Unlucky! The number I'm thinking off is smaller")
-            print("Try again... please enter a number:")
-            user_guessed_number = int(input())
-            number_of_guesses += 1
+            
+        print("Try again... please enter a number:")
+        user_guessed_number = int(input())
+        number_of_guesses += 1
 
     print(f"Congrats! You guessed right in {number_of_guesses} guesses, the number I am thinking off is {random_number}")
